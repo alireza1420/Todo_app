bee4732cb46f51b68e64e9f6f6ed6fe4180afcd4
alireza1420 <alirezafaghihi1383@gmail.com>
2024-05-01 00:30:09 +0330

    Saving data

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ const userDateInput=document.getElementById("date-input");
 const addButton=document.getElementById("add-button");
 const alertMessage=document.getElementById( "alert-message" );
 
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const saveToLocalStorage=()=>{
+
+   localStorage.setItem('todos', JSON.stringify(todos));
+
+}
 
 const showAlert= (message, type) =>{
     alertMessage.innerHTML="";
@@ -15,18 +21,25 @@ const showAlert= (message, type) =>{
 
 }
 
+const generateId = () => {
+
+    return Math.round(Math.random() * Math.random() * Math.pow(10,15));
+}
+
 const addHandler=() => {
 
     const task= userTaskInput.value;
     const date = userDateInput.value;
     const todo={
-        task:task,
-        date:date,
+        id: generateId(),
+        task,
+        date,
         completed:false,
       
         }
         if(task){
             todos.push(todo);
+            saveToLocalStorage();
             userTaskInput.value="";
             userDateInput.value="";
             Swal.fire({
@@ -36,6 +49,7 @@ const addHandler=() => {
                 showConfirmButton: false,
                 timer: 1500
               });
+              console.log(todos);
             }else{
                 Swal.fire({
                     icon: "error",
